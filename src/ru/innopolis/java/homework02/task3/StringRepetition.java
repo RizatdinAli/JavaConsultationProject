d81dfd0b7e229a2f8cf9c3367c91a76c556c69da
RizatdinAli <alirizatdinov@gmail.com>
2023-11-12 20:55:07 +0300

    add homework02 task

---
+++ src/ru/innopolis/java/homework02/task3/StringRepetition.java
@@ -0,0 +1,29 @@
+package ru.innopolis.java.homework02.task3;
+
+import java.util.Scanner;
+
+public class StringRepetition {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Исходная строка: ");
+        String originalString = scanner.nextLine();
+
+        System.out.print("Сколько раз вывести строку? ");
+        int repetitionCount = scanner.nextInt();
+
+        String repeatedString = repeatString(originalString, repetitionCount);
+
+        System.out.println("После повторения " + repetitionCount + " раз: " + repeatedString);
+
+        scanner.close();
+    }
+
+    public static String repeatString(String str, int count) {
+        StringBuilder repeatedString = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            repeatedString.append(str);
+        }
+        return repeatedString.toString();
+    }
+}
