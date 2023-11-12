28662b6b516cd8b70f4964a4ebec32480829c4b7
RizatdinAli <alirizatdinov@gmail.com>
2023-11-12 20:54:50 +0300

    add homework01 task

---
+++ src/ru/innopolis/java/homework01/Main.java
@@ -0,0 +1,12 @@
+package ru.innopolis.java.homework01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("2   0   2   3");
+        int num01 = 2;
+        int num02 = 0;
+        int num03 = 2;
+        int num04 = 3;
+        System.out.printf("%d   %d   %d   %d", num01, num02, num03, num04);
+    }
+}
