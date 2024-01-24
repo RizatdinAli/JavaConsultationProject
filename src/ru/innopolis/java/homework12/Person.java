9fc48f5d74c9f9bc6cab2826a8aaa29344e27f5f
RizatdinAli <148482188+RizatdinAli@users.noreply.github.com>
2024-01-24 22:51:44 +0300

    Добавленно ДЗ homework12

---
+++ src/ru/innopolis/java/homework12/Person.java
@@ -0,0 +1,69 @@
+package ru.innopolis.java.homework12;
+
+public class Person {
+    private String lastName;
+    private String firstName;
+    private String middleName;
+    private String birthDate;
+    private String phoneNumber;
+    private char gender;
+    private int age;
+
+    // Геттеры
+    public String getLastName() {
+        return lastName;
+    }
+
+    // Сеттеры
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
